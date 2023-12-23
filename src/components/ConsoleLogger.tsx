0f1778a56192e1bb76bc5bf03ec2b2dbab24aa91
maormenashe <maormenashe2@gmail.com>
2023-12-23 17:55:19 +0200

    add ts config and update ConsoleLogger

--- src/components/ConsoleLogger.tsx
+++ src/components/ConsoleLogger.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import { useState } from "react";
 
 export const ConsoleLogger = () => {
-  return <div>ConsoleLogger</div>;
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+    </>
+  );
 };
